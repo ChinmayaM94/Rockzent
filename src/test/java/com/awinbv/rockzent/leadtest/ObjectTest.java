6087fc64afdd31b02ca5b509a41101cd938a0b57
ChinmayaM94 <murthychinmaya@gmail.com>
2023-03-27 12:24:06 +0530

    new changes

--- src/test/java/com/awinbv/rockzent/leadtest/ObjectTest.java
+++ src/test/java/com/awinbv/rockzent/leadtest/ObjectTest.java
@@ -9,6 +9,7 @@ public class ObjectTest {
 	@Test(dataProvider = "dp", dataProviderClass = ExcelUtility.class)
 	public void objectTest(String first, String second) throws Throwable {
 		ExcelUtility eLib = new ExcelUtility();
+		System.out.println("Another New lines added");
 		//eLib.getMultipleDataFromExcel("LeadsModule", 1, 1);
 	}
 
